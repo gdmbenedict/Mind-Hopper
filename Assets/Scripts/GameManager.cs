f48ff37a0b1a5212c0dad6a9ca6abbb9d03a89d2
Matthieu Benedict <97464794+Twitchton@users.noreply.github.com>
2024-04-27 13:41:06 -0300

    Created Basic GameManager
    created a basic game manager for use in loading scenes. Other functionality not added yet.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Method to load a scene.
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
